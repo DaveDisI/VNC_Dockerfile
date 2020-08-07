0449c8888409baead91a3a4231199ec1d33c868e
DaveDLeague <dave.dunn@jointheleague.org>
2020-08-07 09:25:24 -0700

    Update desktop_setup.sh

--- desktop_setup.sh
+++ desktop_setup.sh
@@ -11,6 +11,16 @@ apt-get update \
     && apt-get install -y python3-pil \
     && apt-get install -y python3-pil.image \
     && apt-get install -y python3-pil.imagetk \
+    && printf .local/share/umake/ide/eclipse | umake ide eclipse \
+    && echo "[Desktop Entry]" > /usr/share/applications/eclipse-java.desktop \
+    && echo "Version=1.0" >> /usr/share/applications/eclipse-java.desktop \
+    && echo "Type=Application" >> /usr/share/applications/eclipse-java.desktop \
+    && echo "Name=Eclipse" >> /usr/share/applications/eclipse-java.desktop \
+    && echo "Icon=/headless/.local/share/umake/ide/eclipse/java.png" >> /usr/share/applications/eclipse-java.desktop \
+    && echo "Exec=/headless/.local/share/umake/ide/eclipse/eclipse" >> /usr/share/applications/eclipse-java.desktop \
+    && echo "Terminal=false" >> /usr/share/applications/eclipse-java.desktop \
+    && echo "Categories=Development;IDE;Programming" >> /usr/share/applications/eclipse-java.desktop \
+    && rm .local/share/applications/eclipse-java.desktop \
     && wget -O /headless/.config/bg_sakuli.png https://github.com/jointheleague/league-images/blob/master/League%20desktop.png?raw=true \
     && add-apt-repository ppa:ubuntu-mozilla-security/ppa \
     && apt-get update \
