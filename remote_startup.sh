63ccf29e147d5be498bf05ad386c659258b8a7f3
DaveDLeague <dave.dunn@jointheleague.org>
2020-08-17 11:16:55 -0700

    Update remote_startup.sh

--- remote_startup.sh
+++ remote_startup.sh
@@ -1,3 +1,4 @@
+docker build --no-cache -t student-environment2 .
 docker stop $(docker ps -a -q)
 docker rm &(docker ps -a -q)
 docker run -d -p 5901:5901 -p 6901:6901 -p 3000:3000 -p 8080:8080 student-environment2
